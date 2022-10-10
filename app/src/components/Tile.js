b5c984cb396e8a9ba2bddb7cbf78a49986a9a50b
bent_shrimp <park-jinwoo03@naver.com>
2022-10-11 07:33:00 +0900

    got it

--- app/src/components/Tile.js
+++ app/src/components/Tile.js
@@ -2,47 +2,53 @@ import kind from "@enact/core/kind";
 import Proptypes from "prop-types";
 import Button from "@enact/sandstone/Button";
 import css from "./Tile.module.less";
-import thumbnail from "../../icon.jpeg";
 import { useState, useEffect } from "react";
-import LS2Request from '@enact/webos/LS2Request';
+import LS2Request from "@enact/webos/LS2Request";
+import thumbnail1 from "../../resources/1.png";
+import thumbnail2 from "../../resources/2.png";
+import thumbnail3 from "../../resources/3.png";
+import thumbnail4 from "../../resources/4.png";
+import thumbnail5 from "../../resources/5.png";
 
 const bridge = new LS2Request();
 
-
-const Btn = ({app}) => {
+const Btn = ({ app }) => {
   const [toggle, chToggle] = useState(false);
   let app_name;
-  if(app == "배달") {
-    app_name = "com.delivery.app_1.0.0_all.ipk"
+  if (app == "배달") {
+    app_name = "com.delivery.app_1.0.0_all.ipk";
   } else if (app == "차량") {
-    app_name = "com.registercar.app_1.0.0_all.ipk"
-  } else if (app == "cctv"){
-    app_name = "com.cctv.app_1.0.0_all.ipk"
-  } else if (app == "가전제어"){
-    app_name = "com.control.app_1.0.0_all.ipk"
-  } else if (app == "운동보조"){
-    app_name = "com.exercise.app_1.0.0_all.ipk"
+    app_name = "com.registercar.app_1.0.0_all.ipk";
+  } else if (app == "cctv") {
+    app_name = "com.cctv.app_1.0.0_all.ipk";
+  } else if (app == "가전제어") {
+    app_name = "com.control.app_1.0.0_all.ipk";
+  } else if (app == "운동보조") {
+    app_name = "com.exercise.app_1.0.0_all.ipk";
   }
 
   useEffect(() => {
-    console.log(app)
-    if (toggle){
-      appInstall(app_name)
+    console.log(app);
+    if (toggle) {
+      appInstall(app_name);
     }
-  }, [toggle])
+  }, [toggle]);
 
   function appInstall(app) {
-		console.log("[install] " + app);
-		var lsRequest = {
-			service:"luna://com.appstore.app.service",
-			method:"install",
-			parameters: {"app" : app},
-			onSuccess: (msg) => {
-				console.log(msg.message);
-			},
-			onFailure: (msg) => {console.log(msg);console.log("error");},
-		}
-		bridge.send(lsRequest);
+    console.log("[install] " + app);
+    var lsRequest = {
+      service: "luna://com.appstore.app.service",
+      method: "install",
+      parameters: { app: app },
+      onSuccess: (msg) => {
+        console.log(msg.message);
+      },
+      onFailure: (msg) => {
+        console.log(msg);
+        console.log("error");
+      },
+    };
+    bridge.send(lsRequest);
   }
 
   return (
@@ -54,13 +60,21 @@ const Btn = ({app}) => {
   );
 };
 
+const icons = {
+  배달: thumbnail1,
+  차량: thumbnail2,
+  cctv: thumbnail3,
+  가전제어: thumbnail4,
+  운동보조: thumbnail5,
+};
+
 const descs = {
   배달: "배달 물품 도난 방지를 위해 어쩌구 저쩌구 이러쿵 저러쿵..",
   차량: "가정에 손님이 방문할 때 거주민이 아닌 차량이라서 별도의 인증 절차를 걸쳐야 하는 번거러움을 해결 ...",
   cctv: "그냥 cctv로 방법에 유용할듯 대충 뭐 그런거임",
   가전제어:
     "집이 넓으면 월패드에서 전등, 창문 뭐 그런거 작동할 수 있음. 기상 특보랑 게시판에서 나오는 이벤트에 맞춰서 예약도 가능함.",
-  운동보조: "카메라 api가 하자 있어서 삽질 개같이 함",
+  운동보조: "홈트족들을 위한 월패드 운동 보조 앱",
 };
 
 const TileBase = kind({
@@ -88,7 +102,7 @@ const TileBase = kind({
           <div className={css.icon}>
             <img
               className={css.img}
-              src={thumbnail}
+              src={icons[children]}
               width={size}
               height={size}
             />
